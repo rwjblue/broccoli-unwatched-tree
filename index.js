005e15f6597cd9d94152a0a995be4d94bf94102d
Robert Jackson <robert.w.jackson@me.com>
2014-05-11 22:24:31 -0400

    Allow usage without `new`.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Originally implemented by @joliss in `broccoli-bower`
 // https://github.com/joliss/broccoli-bower/blob/ea0eec5c5fa736dc3744f3a7bb0b304b7ac9976e/index.js#L10-L12
 
-function UnwatchedTree (dir) { 
+function UnwatchedTree (dir) {
+  if (!(this instanceof UnwatchedTree)) { return new UnwatchedTree(dir); }
+
   this.dir = dir;
   this.description = 'Unwatched - ' + dir;
 }
