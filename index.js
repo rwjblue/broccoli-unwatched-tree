02bb461980f5e171fbdaf4d575a3608b0de7acb7
Robert Jackson <robert.w.jackson@me.com>
2014-05-11 22:11:41 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+// Originally implemented by @joliss in `broccoli-bower`
+// https://github.com/joliss/broccoli-bower/blob/ea0eec5c5fa736dc3744f3a7bb0b304b7ac9976e/index.js#L10-L12
+
+function UnwatchedTree (dir) { 
+  this.dir = dir;
+  this.description = 'Unwatched - ' + dir;
+}
+
+UnwatchedTree.prototype.read = function (readTree) { return this.dir };
+UnwatchedTree.prototype.cleanup = function () { };
+
+module.exports = UnwatchedTree;
