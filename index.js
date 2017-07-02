1b6cd489a307f1a3ebd94eabb0525fb3fbc42aec
Robert Jackson <rjackson@linkedin.com>
2017-07-02 11:25:16 -0400

    Extend from broccoli-source.
    Supports both `new` and `new`-less invocation.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // please use broccoli-source instead
 var broccoliSource = require('broccoli-source');
 var UnwatchedDir = broccoliSource.UnwatchedDir;
-module.exports = function(path, options){
-	return new UnwatchedDir(path, options);
+
+function UnwatchedTree(path, options){
+  if (!(this instanceof UnwatchedTree)) { return new UnwatchedTree(path, options); }
+
+  UnwatchedDir.call(this, path, options);
 };
+UnwatchedTree.prototype = Object.create(UnwatchedDir.prototype);
+UnwatchedTree.prototype.constructor = UnwatchedTree;
+
+module.exports = UnwatchedTree;
